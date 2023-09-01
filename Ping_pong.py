aa3af0876a25127d64e6e77d2b136595aaa00ee2
Banan4ik132 <sulimam132@gmail.com>
2023-09-01 18:15:08 +0300

    2

--- Ping_pong.py
+++ Ping_pong.py
@@ -1,4 +1,5 @@
 import pygame
+from random import uniform
 
 class Board(pygame.Rect):
     def __init__(self, x , y, widht, height, color, speed, name= "player"):
@@ -8,10 +9,69 @@ class Board(pygame.Rect):
         self.NAME = name 
         self.SCORE = 0
         self.MOVE = {"UP": False, "DOWN": False}
+        self.FONT = pygame.font.SysFont("Conic Sans MS", 40)
 
     def move(self, window):
-        if self.MOVE["UP"]:
+        if self.MOVE["UP"] and self.y > 0:
             self.y -= self.SPEED
-        elif self.MOVE["DOWN"]:
+        elif self.MOVE["DOWN"] and self.y + self.height < 500:
             self.y += self.SPEED
         pygame.draw.rect(window, self.COLOR, self)
+  
+def blit_player_score(window, player_left, player_right, setting_win):
+    render_score_right = player_right.FONT.render(str(player_right.SCORE), True, (58, 134, 221))
+    render_score_left = player_left.FONT.render(str(player_left.SCORE), True, (58, 134, 221))
+    render_name_right = player_right.FONT.render(player_right.NAME, True, (58, 134, 221))
+    render_name_left = player_left.FONT.render(player_right.NAME, True, (58, 134, 221))
+
+
+    window.blit(render_score_left, (setting_win[0] // 2 - 50, 5))
+    window.blit(render_score_right, (setting_win[0] // 2 + 20, 5))
+    window.blit(render_name_left, (5, 5))
+    window.blit(render_name_right, (setting_win[0] - player_right.FONT.size(player_right.NAME)[0] - 5, 5))
+    
+
+
+class Ball(pygame.Rect):
+    def __init__(self, x , y, radius, color, speed):
+        super().__init__(x, y, radius * 2, radius * 2)
+        self.COLOR = color
+        self.SPEED = speed
+        self.SPEED_X = self.SPEED
+        self.SPEED_Y = 0
+        self.RADIUS = radius
+        self.COUNT = 0
+
+    def move(self ,window, player_left, player_right):
+        pygame.draw.circle(window, self.COLOR, (self.x + self.RADIUS, self.y + self.RADIUS), self.RADIUS)
+        
+        if self.y < 0 or self.y + self.height > 500:
+            self.SPEED_Y *= -1
+        if self.colliderect(player_left) and self.COUNT > 3:
+            self.COUNT = 0
+            if player_left.MOVE["UP"]:
+                self.SPEED_Y = uniform(self.SPEED // 2, self.SPEED)
+                coef = ((self.SPEED_X ** 2+ self.SPEED_Y ** 2) ** 0.5) / self.SPEED
+                self.SPEED_X /= coef
+                self.SPEED_Y *= -1
+            elif player_left.MOVE["DOWN"]:
+                self.SPEED_Y = uniform(self.SPEED // 2, self.SPEED)
+                coef = ((self.SPEED_X ** 2+ self.SPEED_Y ** 2) ** 0.5) / self.SPEED
+                self.SPEED_X /= coef
+            self.SPEED_X *= -1
+        elif self.colliderect(player_right):
+            self.COUNT = 0
+            if player_right.MOVE["UP"]:
+                self.SPEED_Y = uniform(self.SPEED // 2, self.SPEED)
+                coef = ((self.SPEED_X ** 2+ self.SPEED_Y ** 2) ** 0.5) / self.SPEED
+                self.SPEED_X /= coef
+                self.SPEED_Y *= -1
+            elif player_right.MOVE["DOWN"]:
+                self.SPEED_Y = uniform(self.SPEED // 2, self.SPEED)
+                coef = ((self.SPEED_X ** 2+ self.SPEED_Y ** 2) ** 0.5) / self.SPEED
+                self.SPEED_X /= coef
+            self.SPEED_X *= -1
+
+        self.x += self.SPEED_X
+        self.y += self.SPEED_Y
+        self.COUNT += 1
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 from Ping_pong import *
+from PyQt5.QtWidgets import QInputDialog, QApplication
 
 pygame.init()
 
+app = QApplication([])
 setting_win =  (700, 500)
 setting_board = (30, 250)
 
@@ -11,21 +13,44 @@ pygame.display.set_caption("PONG")
 
 player_left = Board(10, setting_win[1] // 2 - setting_board[1] // 2, setting_board[0], setting_board[1], (255,255,255), 5)
 player_right = Board(setting_win[0] - setting_board[0]- 10, setting_win[1] // 2 - setting_board[1] // 2, setting_board[0], setting_board[1], (255,255,255), 5)
+player_left.NAME = QInputDialog().getText(QInputDialog(), "PING-PONG", "Введіть ім'я лівого гравця. До 7 символов!")[0][:7].lower()
+player_right.NAME = QInputDialog().getText(QInputDialog(), "PING-PONG", "Введіть ім'я правого гравця. До 7 символов!")[0][:7].lower()
+ball = Ball(setting_win[0] // 2 - 20, setting_win[1] // 2, 20, (220,155,115), 5)
 
 clock = pygame.time.Clock()
 
 game = True
 while game:
     window.fill((0, 0, 0))
-
+    blit_player_score(window, player_left, player_right, setting_win)
     player_left.move(window)
     player_right.move(window)
+    ball.move(window, player_left, player_right)
 
     pygame.draw.line(window, (255, 255, 255), (setting_win[0] // 2, 0), (setting_win[0] // 2, setting_win[1]), 5)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                player_left.MOVE["UP"] = True
+            elif event.key == pygame.K_s:
+                player_left.MOVE["DOWN"] = True
+            if event.key == pygame.K_UP:
+                player_right.MOVE["UP"] = True
+            elif event.key == pygame.K_DOWN:
+                player_right.MOVE["DOWN"] = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                player_left.MOVE["UP"] = False
+            elif event.key == pygame.K_s:
+                player_left.MOVE["DOWN"] = False
+            if event.key == pygame.K_UP:
+                player_right.MOVE["UP"] = False
+            elif event.key == pygame.K_DOWN:
+                player_right.MOVE["DOWN"] = False
+            
 
     clock.tick(60)
     pygame.display.flip()
